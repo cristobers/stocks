ba426ad2a7cd675f499ebfa5dfacd11f493e9eb6
cristobers <72763707+cristobers@users.noreply.github.com>
2024-04-08 19:04:40 +0100

    Added the ability to buy, sell and query prices from the DB with the Discord bot.

--- discord bot/extensions/stocks.py
+++ discord bot/extensions/stocks.py
@@ -1,4 +1,4 @@
-import discord, sqlite3, socket
+import discord, sqlite3, socket, os
 from json import loads
 from discord.ext import commands
 
@@ -22,20 +22,132 @@ def connect_to_stocks(stock: str):
         s.sendall(command)
         return s.recv(1024).decode()
 
+def is_user_in_db(user_id):
+    print(os.getcwd())
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute("""
+        SELECT user_id from users
+        WHERE user_id = ?
+    """, (user_id, ))
+    res = res.fetchone()
+    return res != None
+
+def get_user_info(user_id):
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute("""
+        SELECT * from users
+        WHERE user_id = ?
+    """, (user_id, ))
+    res = res.fetchone()
+    return res
+
+def set_user_money(user_id, money):
+    if not is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute("""
+        UPDATE users
+        set money = ?
+        WHERE user_id = ?
+    """, (money, user_id))
+    con.commit()
+
+def setup_user(user_id, starting_money):
+    if is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute("""
+        INSERT INTO users (user_id, money) 
+        VALUES (?, ?)
+    """, (user_id, starting_money))
+    con.commit()
+    
+def give_user_stocks(user_id, stock_name, amount):
+    cur, con = cur_and_con("../stocks.db")
+
+    data = [(user_id, stock_name) for _ in range(amount)]
+    print(data)
+
+    cur.executemany("""
+        INSERT INTO users_to_stocks (user_id, stock_name) 
+        VALUES (?, ?)
+    """, data)
+    con.commit()
+
+def count_user_stocks(user_id, stock_name) -> int:
+    if not is_user_in_db(user_id):
+        return
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute(
+        """
+        SELECT * FROM users_to_stocks
+        WHERE stock_name = ?
+        AND user_id = ?
+        """, (stock_name, user_id))
+    return len(res.fetchall())
+
+def take_user_stocks(user_id, stock_name, amount) -> float:
+    if not is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute(
+        """
+        DELETE FROM users_to_stocks
+        WHERE user_id = ? 
+        AND stock_name = ? 
+        LIMIT ? 
+        """, (user_id, stock_name, amount))
+    con.commit()
+
+def valid_input(n):
+    if n <= 0:
+        return False 
+    return True
+
 @commands.hybrid_command(name="buy", description="Buys a stock")
 async def buy(ctx, stock, amount):
-    """
-    if (user has enough money):
-        take away price of stock from user
-        add stock to stock_to_user db
-    else:
-        say("you dont have enough go away!")
-    """
-    data = loads(
-        connect_to_stocks(stock)
-    )
+    user_id = ctx.author.id
+    setup_user(user_id, 10_000)
+
+    data = loads(connect_to_stocks(stock))
+    _, user_money = get_user_info(user_id)
+
     name, mp, mh, ml = format(data)
-    await ctx.send(f"{ctx.author.display_name} is buying {amount} {name} for {float(mp)*int(amount)}")
+    user_money = '{0:.2f}'.format(user_money)
+    mp, amount, user_money = float(mp), int(amount), float(user_money)
+
+    if name == "None":
+        await ctx.send("No stock found with this name.")
+        return
+
+    if user_money < (mp * amount) or user_money <= 0:
+        await ctx.send(f"You cannot afford this. You only have `{user_money}`, this would cost you `{'{:.2f}'.format(mp*amount)}`")
+        return
+
+    if not valid_input(amount):
+        await ctx.send("Can't buy <= 0 shares")
+        return
+
+    for i in range(amount):
+        if user_money < mp:
+            break
+        user_money -= mp 
+
+    if user_money <= 0:
+        # i can forsee this maybe being an issue, so ill put this here just incase
+        user_money = 0
+
+    buying_price = float (
+        '{:.2f}'.format(mp * int(amount))
+    )
+
+    user_money = '{0:.2f}'.format(user_money)
+    print(user_money, i)
+    set_user_money(user_id, user_money)
+
+    # now give the user the stocks they bought
+    give_user_stocks(user_id, name, amount)
+    await ctx.send(f"{ctx.author.display_name} is buying {amount} {name} for {buying_price}")
 
 @commands.hybrid_command(name="query", description="querys a stock")
 async def query(ctx, stock):
@@ -45,11 +157,18 @@ async def query(ctx, stock):
         connect_to_stocks(stock)
     )
 
+    name, _, _, _ = format(data)
+    if name == "None":
+        await ctx.send("No stock found with this name.")
+        return
+
     # TODO: make this an embed instead.
     await ctx.send(f"{data['name']} is currently at ${data['market_price']}")
 
 @commands.hybrid_command(name="sell", description="sells a stock")
 async def sell(ctx, stock, amount):
+    user_id = ctx.author.id
+    setup_user(user_id, 10_000)
     """
     if (user has stock in stock_to_user db):
         delete that stock entry
@@ -61,7 +180,32 @@ async def sell(ctx, stock, amount):
         connect_to_stocks(stock)
     )
     name, mp, mh, ml = format(data)
-    await ctx.send(f"{ctx.author.display_name} is selling {amount} {name} for {float(mp)*int(amount)}")
+    user_has = count_user_stocks(user_id, name)
+
+    if name == "None":
+        await ctx.send("No stock found with this name.")
+        return
+
+    amount = int(amount)
+
+    if not valid_input(amount):
+        await ctx.send(f"You can't sell <= 0 shares.")
+        return
+
+    if user_has < amount:
+        await ctx.send(f"You can't sell that many, you only have `{user_has}` shares")
+        return
+
+    curr_user_amount = int(get_user_info(user_id)[1])
+
+    final_amount = float(
+        '{:.2f}'.format(mp * amount)
+    )
+
+    print(user_id, name, amount)
+    take_user_stocks(user_id, name, amount)
+    set_user_money(user_id, curr_user_amount + final_amount)
+    await ctx.send(f"{ctx.author.display_name} is selling {amount} {name} for {final_amount}")
 
 async def setup(bot):
     bot.add_command(buy)
