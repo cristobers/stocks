4b06a0a8e6616ea8e491ceb044eada67341b0695
cristobers <72763707+cristobers@users.noreply.github.com>
2024-04-10 22:10:02 +0100

    Added embeds to the commands and abstracted away the database functions

---
+++ discord bot/extensions/db.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+def cur_and_con(db_name):
+    con = sqlite3.connect(db_name)
+    return con.cursor(), con
+
+def is_user_in_db(user_id):
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute("""
+        SELECT user_id from users
+        WHERE user_id = ?
+    """, (user_id, ))
+    res = res.fetchone()
+    return res != None
+
+def get_user_info(user_id):
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute("""
+        SELECT * from users
+        WHERE user_id = ?
+    """, (user_id, ))
+    res = res.fetchone()
+    return res
+
+def set_user_money(user_id, money):
+    print(user_id, money)
+    if not is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute("""
+        UPDATE users
+        set money = ?
+        WHERE user_id = ?
+    """, (money, user_id))
+    con.commit()
+
+def setup_user(user_id, starting_money):
+    if is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute("""
+        INSERT INTO users (user_id, money) 
+        VALUES (?, ?)
+    """, (user_id, starting_money))
+    con.commit()
+    
+def give_user_stocks(user_id, stock_name, amount):
+    cur, con = cur_and_con("../stocks.db")
+
+    data = [(user_id, stock_name) for _ in range(amount)]
+
+    cur.executemany("""
+        INSERT INTO users_to_stocks (user_id, stock_name) 
+        VALUES (?, ?)
+    """, data)
+    con.commit()
+
+def count_user_stocks(user_id, stock_name) -> int:
+    if not is_user_in_db(user_id):
+        return
+    cur, _ = cur_and_con("../stocks.db")
+    res = cur.execute(
+        """
+        SELECT * FROM users_to_stocks
+        WHERE stock_name = ?
+        AND user_id = ?
+        """, (stock_name, user_id))
+    return len(res.fetchall())
+
+def take_user_stocks(user_id, stock_name, amount) -> float:
+    if not is_user_in_db(user_id):
+        return
+    cur, con = cur_and_con("../stocks.db")
+    cur.execute(
+        """
+        DELETE FROM users_to_stocks
+        WHERE user_id = ? 
+        AND stock_name = ? 
+        LIMIT ? 
+        """, (user_id, stock_name, amount))
+    con.commit()
--- discord bot/extensions/stocks.py
+++ discord bot/extensions/stocks.py
@@ -1,6 +1,7 @@
-import discord, sqlite3, socket, os
+import discord, socket, os
 from json import loads
 from discord.ext import commands
+import extensions.db as db
 
 host, port = '127.0.0.1', 7690
 
@@ -11,10 +12,6 @@ def format(data):
     market_day_low = data['market_day_low']
     return name, market_price, market_day_high, market_day_low
 
-def cur_and_con(db_name):
-    con = sqlite3.connect(db_name)
-    return con.cursor(), con
-
 def connect_to_stocks(stock: str):
     command = bytes(f"QUERY {stock}\r\n", encoding="utf8")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -22,94 +19,26 @@ def connect_to_stocks(stock: str):
         s.sendall(command)
         return s.recv(1024).decode()
 
-def is_user_in_db(user_id):
-    cur, _ = cur_and_con("../stocks.db")
-    res = cur.execute("""
-        SELECT user_id from users
-        WHERE user_id = ?
-    """, (user_id, ))
-    res = res.fetchone()
-    return res != None
-
-def get_user_info(user_id):
-    cur, _ = cur_and_con("../stocks.db")
-    res = cur.execute("""
-        SELECT * from users
-        WHERE user_id = ?
-    """, (user_id, ))
-    res = res.fetchone()
-    return res
-
-def set_user_money(user_id, money):
-    print(user_id, money)
-    if not is_user_in_db(user_id):
-        return
-    cur, con = cur_and_con("../stocks.db")
-    cur.execute("""
-        UPDATE users
-        set money = ?
-        WHERE user_id = ?
-    """, (money, user_id))
-    con.commit()
-
-def setup_user(user_id, starting_money):
-    if is_user_in_db(user_id):
-        return
-    cur, con = cur_and_con("../stocks.db")
-    cur.execute("""
-        INSERT INTO users (user_id, money) 
-        VALUES (?, ?)
-    """, (user_id, starting_money))
-    con.commit()
-    
-def give_user_stocks(user_id, stock_name, amount):
-    cur, con = cur_and_con("../stocks.db")
-
-    data = [(user_id, stock_name) for _ in range(amount)]
-
-    cur.executemany("""
-        INSERT INTO users_to_stocks (user_id, stock_name) 
-        VALUES (?, ?)
-    """, data)
-    con.commit()
-
-def count_user_stocks(user_id, stock_name) -> int:
-    if not is_user_in_db(user_id):
-        return
-    cur, _ = cur_and_con("../stocks.db")
-    res = cur.execute(
-        """
-        SELECT * FROM users_to_stocks
-        WHERE stock_name = ?
-        AND user_id = ?
-        """, (stock_name, user_id))
-    return len(res.fetchall())
-
-def take_user_stocks(user_id, stock_name, amount) -> float:
-    if not is_user_in_db(user_id):
-        return
-    cur, con = cur_and_con("../stocks.db")
-    cur.execute(
-        """
-        DELETE FROM users_to_stocks
-        WHERE user_id = ? 
-        AND stock_name = ? 
-        LIMIT ? 
-        """, (user_id, stock_name, amount))
-    con.commit()
-
 def valid_input(n):
     if n <= 0:
         return False 
     return True
 
+def embed(type, name, mp, amount):
+    # TODO: get seperate embeds for each action
+    embed = discord.Embed(title=f"{type} {name}")
+    embed.add_field(name="Price", value=mp, inline=False)
+    if amount != None:
+        embed.add_field(name="You currently have", value=amount, inline=False)
+    return embed
+
 @commands.hybrid_command(name="buy", description="Buys a stock")
 async def buy(ctx, stock, amount):
     user_id = ctx.author.id
-    setup_user(user_id, 10_000)
+    db.setup_user(user_id, 10_000)
 
     data = loads(connect_to_stocks(stock))
-    _, user_money = get_user_info(user_id)
+    _, user_money = db.get_user_info(user_id)
 
     name, mp, _, _ = format(data)
     user_money = '{0:.2f}'.format(user_money)
@@ -142,11 +71,12 @@ async def buy(ctx, stock, amount):
 
     user_money = float('{0:.2f}'.format(user_money))
     print(f"USER MONEY: {user_money} BUYING PRICE {buying_price} SUM {float(user_money)}")
-    set_user_money(user_id, user_money)
+    db.set_user_money(user_id, user_money)
 
     # now give the user the stocks they bought
-    give_user_stocks(user_id, name, amount)
-    await ctx.send(f"{ctx.author.display_name} is buying {amount} {name} for {buying_price}, you now have {user_money}")
+    db.give_user_stocks(user_id, name, amount)
+    stock_amount = db.count_user_stocks(user_id, name)
+    await ctx.send(embed=embed("Buying", name, mp, stock_amount))
 
 @commands.hybrid_command(name="query", description="querys a stock")
 async def query(ctx, stock):
@@ -161,13 +91,13 @@ async def query(ctx, stock):
         await ctx.send("No stock found with this name.")
         return
 
-    # TODO: make this an embed instead.
-    await ctx.send(f"{data['name']} is currently at ${data['market_price']}")
+    name, mp, _, _ = format(data)
+    await ctx.send(embed=embed("Querying", name, mp, None))
 
 @commands.hybrid_command(name="sell", description="sells a stock")
 async def sell(ctx, stock, amount):
     user_id = ctx.author.id
-    setup_user(user_id, 10_000)
+    db.setup_user(user_id, 10_000)
     """
     if (user has stock in stock_to_user db):
         delete that stock entry
@@ -179,12 +109,11 @@ async def sell(ctx, stock, amount):
         connect_to_stocks(stock)
     )
     name, mp, _, _ = format(data)
-    user_has = count_user_stocks(user_id, name)
+    user_has = db.count_user_stocks(user_id, name)
 
     if name == "None":
         await ctx.send("No stock found with this name.")
         return
-
     amount = int(amount)
 
     if not valid_input(amount):
@@ -195,15 +124,16 @@ async def sell(ctx, stock, amount):
         await ctx.send(f"You can't sell that many, you only have `{user_has}` shares")
         return
 
-    curr_user_amount = int(get_user_info(user_id)[1])
+    curr_user_amount = int(db.get_user_info(user_id)[1])
 
     final_amount = float(
         '{:.2f}'.format(mp * amount)
     )
 
-    take_user_stocks(user_id, name, amount)
-    set_user_money(user_id, curr_user_amount + final_amount)
-    await ctx.send(f"{ctx.author.display_name} is selling {amount} {name} for {final_amount}")
+    db.take_user_stocks(user_id, name, amount)
+    db.set_user_money(user_id, curr_user_amount + final_amount)
+    user_has = db.count_user_stocks(user_id, name)
+    await ctx.send(embed=embed("Selling", name, mp, user_has))
 
 async def setup(bot):
     bot.add_command(buy)
